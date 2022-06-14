bfaaf47ff7ee9f245ff830d46471048a54d44107
Nidhi <nidhisunil2000@gmail.com>
2022-06-14 13:19:25 -0300

    basic file setup

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.set('view engine', 'ejs'); //sets our view engine as ejs
+
+app.get("/", function(req, res){
+  var today = new Date();
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render('list', {day:day});
+});
+
+app.listen(3000, function(){
+  console.log("Server up and running on port 3000");
+});
---
+++ readme.txt
@@ -0,0 +1,6 @@
+To set up->
+Download npm, nodemon
+In terminal, run "npm init"
+Run "npm i express body-parser ejs"
+Run "nodemon app.js"
+Go to localhost:3000/
