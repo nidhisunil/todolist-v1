bba87384e28e9a27ac0a4ac35cf7b3cb4e5d337a
Nidhi <nidhisunil2000@gmail.com>
2022-06-14 13:59:30 -0300

    add css styles

--- app.js
+++ app.js
@@ -3,23 +3,24 @@ const bodyParser = require("body-parser");
 const app = express();
 app.set('view engine', 'ejs'); //sets our view engine as ejs
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public")); //to use static files stored in folder called "public"
 
 
-var newItems = [];
+let newItems = [];
 app.get("/", function(req, res){
-  var today = new Date();
-  var options = {
+  let today = new Date();
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   };
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render('list', {day:day, newItems:newItems});
 });
 
 app.post("/", function(req,res){
-  var newItem = req.body.newItem;
+  let newItem = req.body.newItem;
   newItems.push(newItem);
   res.redirect("/");
 });
