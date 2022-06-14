1d89266bace4b77b5bad97fbb577ac4a7649454e
Nidhi <nidhisunil2000@gmail.com>
2022-06-14 13:43:27 -0300

    basic list rendering done

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 app.set('view engine', 'ejs'); //sets our view engine as ejs
+app.use(bodyParser.urlencoded({extended: true}));
 
+
+var newItems = [];
 app.get("/", function(req, res){
   var today = new Date();
   var options = {
@@ -12,7 +15,13 @@ app.get("/", function(req, res){
   };
   var day = today.toLocaleDateString("en-US", options);
 
-  res.render('list', {day:day});
+  res.render('list', {day:day, newItems:newItems});
+});
+
+app.post("/", function(req,res){
+  var newItem = req.body.newItem;
+  newItems.push(newItem);
+  res.redirect("/");
 });
 
 app.listen(3000, function(){
